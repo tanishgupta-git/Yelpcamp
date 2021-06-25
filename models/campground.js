d38e73068cdc992ffeb531b2d16a13cd1dd50008
tanishgupta-git <tanishgupta8181@gmail.com>
2021-06-25 15:17:25 +0530

    added image upload in cloudinary feature

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,19 @@ const monogoose = require('mongoose');
 const Schema = monogoose.Schema;
 const Review = require('./review')
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String
+});
+
+// we are doing this becuase whenever we call img.thumbnail it will replace it in url
+ImageSchema.virtual('thumbnail').get(function () {
+  return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
   title : String,
-  image: String, 
+  images: [ImageSchema], 
   price : Number,
   description : String,
   location : String,
--- seeds/index.js
+++ seeds/index.js
@@ -28,9 +28,19 @@ const seedDB = async () => {
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             author:'60d1c8d4e6344c4610555e40',
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste cum, cupiditate veritatis dolores molestiae amet ullam aliquam tenetur quos. Consequatur neque repellendus recusandae eum fuga laborum doloribus ratione, aperiam odio?',
-            price:price
+            price,
+            images: [
+                {
+                  url: 'https://res.cloudinary.com/yelpcamptan/image/upload/v1624610796/Yelpcamp/tcghklpqcbufzhsalm4y.jpg',
+                  filename: 'Yelpcamp/tcghklpqcbufzhsalm4y'
+                },
+                {
+                  url: 'https://res.cloudinary.com/yelpcamptan/image/upload/v1624610796/Yelpcamp/gezr7zqiqyw1onx4ud6r.jpg',
+                  filename: 'Yelpcamp/gezr7zqiqyw1onx4ud6r'
+                }
+            ]            
+
         })
         await camp.save();
     }
